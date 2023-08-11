bea748bd6f92b72826fea6d38f43a691c796ad8e
Jesse <henrique.jessex@gmail.com>
2023-08-11 19:16:15 -0300

    update Docker

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const server = require('http').createServer();
 const app = express();
-const PORT = 3000;
 
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname});
@@ -9,7 +8,13 @@ app.get('/', function(req, res) {
 
 server.on('request', app);
 
-server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+process.on('SIGINT', () => {
+  server.close(() => {
+    shutdownDB(); 
+  });
+});
+
+server.listen(3000, function () { console.log('Listening on 3000'); });
 
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
@@ -20,6 +25,10 @@ wss.on('connection', function connection(ws) {
   const numClients = wss.clients.size;
 
   console.log('clients connected: ', numClients);
+
+  // Log number of visitors at current moment
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))`);
 
   wss.broadcast(`Current visitors: ${numClients}`);
 
@@ -33,7 +42,7 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.on('error', function error() {
-    //
+ 
   });
 });
 
@@ -50,3 +59,31 @@ wss.broadcast = function broadcast(data) {
 };
 /** End Websocket **/
 
+/** Database stuff **/
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+// .seralize ensures DB is set up before any queries
+db.serialize(() => {
+    db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+      console.log(row);
+    });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('shutting down DB');
+  db.close();
+}
+
+
+
+/** End Database stuff **/
